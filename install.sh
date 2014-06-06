6caa4b72126e067daa8f40f246ae44fac35b1064
Ondrej Sika <ondrej@ondrejsika.com>
2014-06-06 11:51:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 sudo git clone https://github.com/frrp/patent_parser.git /sucker
 
+cd /sucker
+
 sudo apt-get update
 sudo apt-get install python-pip python-dev postgresql-server-dev-9.3 postgresql-9.3
 
@@ -12,5 +14,5 @@ sudo pip install -r requirements.txt
 sudo python manage.py syncdb --noinput
 sudo python manage.py migrate sucker
 
-sudo cp /sucker/init.d/sucker /etc/init.d/sucker
+sudo cp init.d/sucker /etc/init.d/sucker
 
